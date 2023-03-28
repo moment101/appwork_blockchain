95fad7fae40f8583af8b0aeb1fca61fb7353e47b
jon <u8812012moment@gmail.com>
2023-03-28 12:04:43 +0800

    hw4

---
+++ Smart_Contract_Youtue/Library.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+library Math {
+    function max(uint x, uint y) internal pure returns (uint) {
+        return x >= y ? x : y;
+    }
+}
+
+contract Test {
+    function textMax(uint x, uint y) external pure returns (uint) {
+        return Math.max(x, y);
+    }
+}
